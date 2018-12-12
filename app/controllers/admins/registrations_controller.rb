95295dba23f6bce6e2f8d3e37f2be0a15ee4f207
as.ja22 <as_ja22@yahoo.com>
2018-12-12 19:57:13 +0800

    simple corrections

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -62,7 +62,11 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-   
+  protected
+
+    def after_sign_up_path_for(resource)
+      admin_home_path
+    end
    
   protected
 
--- app/controllers/agents/registrations_controller.rb
+++ app/controllers/agents/registrations_controller.rb
@@ -65,6 +65,12 @@ class Agents::RegistrationsController < Devise::RegistrationsController
   
   protected
 
+    def after_sign_up_path_for(resource)
+      agent_home_path
+    end
+  
+  protected
+
      def configure_permitted_parameters
          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
