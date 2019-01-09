39bf074156527f3fe7719bfa5646c5ddd282c76a
as.ja22 <as_ja22@yahoo.com>
2019-01-09 10:54:28 +0800

    updates in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,12 @@ Package.populate 64 do |package|
   package.name = Populator.words(1..5).titleize
   package.days = 3..8
   package.nights = 2..7
+  package.departure = "2019-03-28"
+  package.country_id = 1..6
+  package.province_id = 1
   package.itinerary = Populator.paragraphs(3)
-  package.price = 600..2500
   package.agent_id = 1..2
+  package.price = 600..2500
   package.includes = Populator.sentences(1)
   package.approved = false
 end
