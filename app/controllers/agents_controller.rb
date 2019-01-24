1238d8e3d122362ed44d6ab520acfd423ec4f5c0
as.ja22 <as_ja22@yahoo.com>
2019-01-24 13:55:06 +0800

    agent error cleared

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -11,17 +11,17 @@ class AgentsController < ApplicationController
   end
   
   def show
-    if current_agent
+    #if current_agent
      @agent = Agent.find(params[:id])
-    elsif current_admin
-      cont=Rails.application.routes.recognize_path(request.referrer) 
-      if cont[:controller] == 'agents'
-        @agent = Agent.find(params[:id])
-      elsif cont[:controller] == 'packages'
-        @package = Package.find(params[:id])
-        @agent = @package.agent
-      end
-    end
+    #elsif current_admin
+    #  cont=Rails.application.routes.recognize_path(request.referrer) 
+    #  if cont[:controller] == 'agents'
+     #   @agent = Agent.find(params[:id])
+    #  elsif cont[:controller] == 'packages'
+     #   @package = Package.find(params[:id])
+      #  @agent = @package.agent
+      #end
+      #end
   end
   
   def activate
